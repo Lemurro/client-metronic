24503ce23e6c597dc08fac31fa6489a287de12a4
DimNS <atomcms@ya.ru>
2018-06-05 14:08:52 +0500

    Создание файла rev-manifest.json для работы симфони-компонента Asset

--- gulpfile.js
+++ gulpfile.js
@@ -3,88 +3,30 @@ var cleanCSS = require('gulp-clean-css'); // Минификация CSS
 var concat   = require('gulp-concat');    // Склейка файлов
 var uglify   = require('gulp-uglify');    // Минификация JS
 var del      = require('del');            // Удаление файлов
-var md5      = require('gulp-md5-plus');  // Подстановка в имени файлов md5-хеша
 var rename   = require('gulp-rename');    // Переименование файлов
+var rev      = require('gulp-rev');       // Версионность файлов
 
 // Сборка проекта для тестирования
-gulp.task('default', ['watcher', 'js', 'css', 'libs']);
+gulp.task('default', ['build', 'watcher.css', 'watcher.js']);
 
 // Сборка проекта для релиза
-gulp.task('build', ['libs'], function () {
-    // app.js
-    del('assets/app_*.js', {
-        force: true
-    });
+gulp.task('build', ['del.old', 'libs', 'select2.lang', 'core', 'app.css', 'app.js']);
 
-    gulp.src('src/js/**/*.js')
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(md5(10))
-        .pipe(gulp.dest('assets'));
-
-    // app.css
-    del('assets/app_*.css', {
-        force: true
-    });
-
-    gulp.src('src/css/*.css')
-        .pipe(concat('app.css'))
-        .pipe(cleanCSS())
-        .pipe(md5(10))
-        .pipe(gulp.dest('assets'));
+gulp.task('watcher.css', function () {
+    return gulp.watch('src/css/*.css', ['app.css']);
 });
 
-gulp.task('watcher', function () {
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('src/css/*.css', ['css']);
+gulp.task('watcher.js', function () {
+    return gulp.watch('src/js/**/*.js', ['app.js']);
 });
 
-gulp.task('js', function () {
-    // app.js
-    del('assets/app_*.js', {
+gulp.task('del.old', function () {
+    return del(['assets/*.css', 'assets/*.js'], {
         force: true
     });
-
-    gulp.src('src/js/**/*.js')
-        .pipe(concat('app.js'))
-        .pipe(md5(10))
-        .pipe(gulp.dest('assets'));
 });
 
-gulp.task('css', function () {
-    // app.css
-    del('assets/app_*.css', {
-        force: true
-    });
-
-    gulp.src('src/css/*.css')
-        .pipe(concat('app.css'))
-        .pipe(md5(10))
-        .pipe(gulp.dest('assets'));
-});
-
-// Перенос необходимых библиотек
 gulp.task('libs', function () {
-    // core.css
-    del('assets/core_*.css', {
-        force: true
-    });
-
-    // core.js
-    del('assets/core_*.js', {
-        force: true
-    });
-
-    gulp.src('bower_components/lemurro-client-metronic-core/dist/core.min.css')
-        .pipe(rename('core.css'))
-        .pipe(md5(10))
-        .pipe(gulp.dest('assets'));
-
-    gulp.src('bower_components/lemurro-client-metronic-core/dist/core.min.js')
-        .pipe(rename('core.js'))
-        .pipe(md5(10))
-        .pipe(gulp.dest('assets'));
-
     var libs = [
         'bower_components/bowser/src/bowser.js',
         'bower_components/jquery-lightajax/dist/jquery.lightajax.min.css',
@@ -94,10 +36,61 @@ gulp.task('libs', function () {
         'bower_components/Template7/dist/template7.min.js'
     ];
 
-    gulp.src(libs)
+    return gulp.src(libs)
         .pipe(gulp.dest('assets/plugins'));
+});
 
-    gulp.src('bower_components/select2/dist/js/i18n/ru.js')
+gulp.task('select2.lang', function () {
+    return gulp.src('bower_components/select2/dist/js/i18n/ru.js')
         .pipe(rename('select2.lang.ru.js'))
         .pipe(gulp.dest('assets/plugins'));
+});
+
+gulp.task('core', function () {
+    return gulp.src(['bower_components/lemurro-client-metronic-core/dist/core.min.css', 'bower_components/lemurro-client-metronic-core/dist/core.min.js'])
+        .pipe(rev())
+        .pipe(gulp.dest('assets'))
+        .pipe(rev.manifest({
+            base : 'assets',
+            merge: true
+        }))
+        .pipe(gulp.dest('assets'));
+});
+
+gulp.task('app.css.del', function () {
+    return del('assets/app-*.css', {
+        force: true
+    });
+});
+
+gulp.task('app.js.del', function () {
+    return del('assets/app-*.js', {
+        force: true
+    });
+});
+
+gulp.task('app.css', ['app.css.del'], function () {
+    return gulp.src('src/css/*.css')
+        .pipe(concat('app.min.css'))
+        .pipe(cleanCSS())
+        .pipe(rev())
+        .pipe(gulp.dest('assets'))
+        .pipe(rev.manifest({
+            base : 'assets',
+            merge: true
+        }))
+        .pipe(gulp.dest('assets'));
+});
+
+gulp.task('app.js', ['app.js.del'], function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest('assets'))
+        .pipe(rev.manifest({
+            base : 'assets',
+            merge: true
+        }))
+        .pipe(gulp.dest('assets'));
 });
