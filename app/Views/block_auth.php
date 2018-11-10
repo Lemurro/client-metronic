a3535fdf71218f7575eab4d6af66c86176722da4
DimNS <atomcms@ya.ru>
2018-11-10 10:05:43 +0500

    fix: Убран атрибут tabindex="-1" у модальных окон, чтобы работал Select2 в режиме multiple

--- app/Views/block_auth.php
+++ app/Views/block_auth.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="js-auth" tabindex="-1" role="dialog">
+<div class="modal fade" id="js-auth" role="dialog">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-body">
