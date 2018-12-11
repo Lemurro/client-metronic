c4e6f2ffce5a6001c38cf2fb69cb4ecfe10099e2
DimNS <atomcms@ya.ru>
2018-12-11 12:08:44 +0500

    refactor: Вынесение кода в ядро

--- src/js/users/100-_collectData.js
+++ src/js/users/100-_collectData.js
@@ -7,32 +7,12 @@
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 users._collectData = function () {
-    var form  = $('#js-user-form');
-    var roles = {};
-
-    form.find('.js-role:checked').each(function () {
-        var elem = $(this);
-        var role = elem.attr('data-role');
-
-        if (role === 'admin') {
-            roles.admin = true;
-        } else {
-            if (!roles.hasOwnProperty(role)) {
-                roles[role] = [];
-            }
-
-            roles[role].push(elem.attr('data-access'));
-        }
-    });
-
-    if (roles.hasOwnProperty('admin')) {
-        roles = {admin: true};
-    }
+    var form = $('#js-user-form');
 
     return {
         id        : form.attr('data-id'),
         auth_id   : form.find('input[name="auth_id"]').val(),
-        roles     : roles,
+        roles     : lemurro.users.getRoles(form),
         info_users: {}
     };
 };
--- src/js/users/200-edit.js
+++ src/js/users/200-edit.js
@@ -12,16 +12,6 @@ users.edit = function (id) {
 
         form.find('input[name="auth_id"]').val(result.data.auth_id);
 
-        form.find('.js-role').prop('checked', false);
-
-        for (var role in result.data.roles) {
-            if (role === 'admin') {
-                form.find('.js-role[data-role="admin"]').prop('checked', true);
-            } else {
-                for (var i in result.data.roles[role]) {
-                    form.find('.js-role[data-role="' + role + '"][data-access="' + result.data.roles[role][i] + '"]').prop('checked', true);
-                }
-            }
-        }
+        lemurro.users.setRoles(form, result.data.roles);
     });
 };
