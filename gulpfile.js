bdb6ee6b43a46e958bf650f05cfe524a594d2008
Дмитрий Щербаков <atomcms@ya.ru>
2019-02-13 14:41:13 +0500

    feat Переход на Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,66 @@
-var gulp        = require('gulp');             // Сам Gulp JS
-var cleanCSS    = require('gulp-clean-css');   // Минификация CSS
-var concat      = require('gulp-concat');      // Склейка css и js файлов
-var del         = require('del');              // Удаление файлов
-var includer    = require('gulp-x-includer');  // Склейка html файлов
-var rev         = require('gulp-rev');         // Версионность файлов
-var revRewrite  = require('gulp-rev-rewrite'); // Внедрение ссылок на css и js файлы в index.html
-var runSequence = require('run-sequence');     // Выполнение gulp-задач по очереди
-var uglify      = require('gulp-uglify');      // Минификация JS
+var gulp       = require('gulp');             // Сам Gulp JS
+var cleanCSS   = require('gulp-clean-css');   // Минификация CSS
+var concat     = require('gulp-concat');      // Склейка css и js файлов
+var del        = require('del');              // Удаление файлов
+var includer   = require('gulp-x-includer');  // Склейка html файлов
+var rev        = require('gulp-rev');         // Версионность файлов
+var revRewrite = require('gulp-rev-rewrite'); // Внедрение ссылок на css и js файлы в index.html
+var uglify     = require('gulp-uglify');      // Минификация JS
 
-var plugins = [];
+var pathsPlugins = [];
 
-// Сборка проекта + watchers
-gulp.task('default', function () {
-    runSequence(
-        'build',
-        ['watch.css', 'watch.js', 'watch.html']
-    );
-});
+// CLEAN
 
-// Сборка проекта
-gulp.task('build', function () {
-    runSequence(
-        'clean',
-        ['htaccess', 'plugins', 'assets', 'fontawesome'],
-        'lemurro',
-        'app.css',
-        'app.js',
-        ['pages.html', 'index.html']
-    );
-});
-
-gulp.task('watch.css', function () {
-    return gulp.watch('src/css/*.css', function () {
-        runSequence(
-            'app.css',
-            'index.html'
-        );
-    });
-});
-
-gulp.task('watch.js', function () {
-    return gulp.watch('src/js/**/*.js', function () {
-        runSequence(
-            'app.js',
-            'index.html'
-        );
-    });
-});
-
-gulp.task('watch.html', function () {
-    return gulp.watch('src/html/**/*.html', ['pages.html', 'index.html']);
-});
-
-gulp.task('clean', function () {
+function clean() {
     return del('build/**', {force: true});
-});
+}
 
-gulp.task('htaccess', function () {
+// WATCHER
+
+function watcherCSS() {
+    return gulp.watch('src/css/*.css', gulp.series(appCSS, indexHTML));
+}
+
+function watcherJS() {
+    return gulp.watch('src/js/**/*.js', gulp.series(appJS, indexHTML));
+}
+
+function watcherHTML() {
+    return gulp.watch('src/html/**/*.html', gulp.parallel(pagesHTML, indexHTML));
+}
+
+// APP
+
+function htaccess() {
     return gulp.src('.htaccess')
         .pipe(gulp.dest('build'));
-});
+}
 
-gulp.task('plugins', function () {
-    return gulp.src(plugins)
-        .pipe(gulp.dest('build/assets/plugins'));
-});
+function plugins(done) {
+    if (pathsPlugins.length > 0) {
+        return gulp.src(pathsPlugins)
+            .pipe(gulp.dest('build/assets/plugins'));
+    } else {
+        done();
+    }
+}
 
-gulp.task('assets', function () {
+function assets() {
     return gulp.src('src/assets/**/*')
         .pipe(gulp.dest('build/assets'));
-});
+}
 
-gulp.task('fontawesome', ['fontawesome.css', 'fontawesome.webfonts']);
-
-gulp.task('fontawesome.css', function () {
+function fontawesomeCSS() {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/css/all.min.css')
         .pipe(gulp.dest('build/assets/fonts/fontawesome-free/css'));
-});
+}
 
-gulp.task('fontawesome.webfonts', function () {
+function fontawesomeWebfonts() {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
         .pipe(gulp.dest('build/assets/fonts/fontawesome-free/webfonts'));
-});
+}
 
-gulp.task('lemurro', function () {
+function lemurro() {
     var files = [
         'node_modules/lemurro-client-metronic-core-frontend/dist/lemurro.min.css',
         'node_modules/lemurro-client-metronic-core-frontend/dist/lemurro.min.js'
@@ -97,9 +74,9 @@ gulp.task('lemurro', function () {
             merge: true
         }))
         .pipe(gulp.dest('build/assets'));
-});
+}
 
-gulp.task('app.css', function () {
+function appCSS() {
     return gulp.src('src/css/*.css')
         .pipe(concat('app.min.css'))
         .pipe(cleanCSS())
@@ -110,9 +87,9 @@ gulp.task('app.css', function () {
             merge: true
         }))
         .pipe(gulp.dest('build/assets'));
-});
+}
 
-gulp.task('app.js', function () {
+function appJS() {
     return gulp.src('src/js/**/*.js')
         .pipe(concat('app.min.js'))
         .pipe(uglify())
@@ -123,15 +100,15 @@ gulp.task('app.js', function () {
             merge: true
         }))
         .pipe(gulp.dest('build/assets'));
-});
+}
 
-gulp.task('pages.html', function () {
+function pagesHTML() {
     return gulp.src('src/html/pages/*.html')
         .pipe(includer())
         .pipe(gulp.dest('build'));
-});
+}
 
-gulp.task('index.html', function () {
+function indexHTML() {
     var manifest = gulp.src('build/rev-manifest.json');
 
     return gulp.src('src/html/index.html')
@@ -140,4 +117,25 @@ gulp.task('index.html', function () {
             manifest: manifest
         }))
         .pipe(gulp.dest('build'));
-});
+}
+
+// VARS
+
+var fontawesome = gulp.parallel(fontawesomeCSS, fontawesomeWebfonts);
+
+// TASKS
+
+gulp.task('build', gulp.series(
+    clean,
+    gulp.parallel(htaccess, plugins, assets, fontawesome),
+    lemurro,
+    appCSS,
+    appJS,
+    gulp.parallel(pagesHTML, indexHTML)
+));
+
+gulp.task('watcher', gulp.series('build', gulp.parallel(
+    watcherCSS,
+    watcherJS,
+    watcherHTML
+)));
