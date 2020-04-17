ef4f40ad0b00c3d28e05e1e52a1ffdc4de884904
Дмитрий Щербаков <atomcms@ya.ru>
2020-04-17 13:33:00 +0500

    feat: Добавлен js-метод users.afterShowData

---
+++ src/js/users/300-afterShowData.js
@@ -0,0 +1,12 @@
+/**
+ * Функция обратного вызова после отображения списка пользователей
+ *
+ * @param {Object} data
+ *
+ * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 17.04.2020
+ */
+users.afterShowData = function (data) {
+
+};
