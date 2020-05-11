0dc45e5cd79a260b453dc3dcf5c0b0aa14eb06e8
Дмитрий Щербаков <atomcms@ya.ru>
2020-05-11 14:08:01 +0500

    fix [Пользователи] В методе "lemurro.users.remove" необходимо удалить неиспользуемый параметр "name"

--- src/js/users/300-remove.js
+++ src/js/users/300-remove.js
@@ -3,13 +3,12 @@
  *
  * @param {integer} id ИД записи
  *
- * @version 26.10.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 11.05.2020
  */
 users.remove = function (id) {
-    var authID = $('#js-users__items').find('tr[data-item-id="' + id + '"] .auth_id').text();
-
-    lemurro.users.remove(id, authID, function (result) {
+    lemurro.users.remove(id, function (result) {
         var container = $('#js-users__items');
 
         container.find('tr[data-item-id="' + result.data.id + '"]').remove();
