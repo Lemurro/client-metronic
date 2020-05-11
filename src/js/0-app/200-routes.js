c7f3b79c65a9c2f878fb7f19221b7e602ad2d62f
Дмитрий Щербаков <atomcms@ya.ru>
2020-05-11 13:55:54 +0500

    feat Добавлен раздел "Мой профиль"

--- src/js/0-app/200-routes.js
+++ src/js/0-app/200-routes.js
@@ -1,79 +1,91 @@
 /**
  * Маршруты
  *
- * @version 24.04.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 11.05.2020
  */
 app.routes = {
     notFound: function (params, query) {
         app.page = {
-            name  : '404',
-            title : 'Страница не найдена',
+            name: '404',
+            title: 'Страница не найдена',
             onLoad: null,
             params: params,
-            query : query
+            query: query,
         };
     },
-    pages   : {
+    pages: {
         // Пользовательские маршруты
         '/example': function (params, query) {
             app.page = {
-                name  : 'example',
-                title : 'Пример',
+                name: 'example',
+                title: 'Пример',
                 onLoad: function () {
                     example.init();
                 },
                 params: params,
-                query : query
+                query: query,
             };
         },
 
         // Справочники
         '/guide/example': function (params, query) {
             app.page = {
-                name  : 'guide', // Всегда guide для всех справочников
-                title : 'Пример | Справочник',
+                name: 'guide', // Всегда guide для всех справочников
+                title: 'Пример | Справочник',
                 onLoad: function () {
                     lemurro.guide.init('example', 'guideExample');
                 },
                 params: params,
-                query : query
+                query: query,
             };
         },
 
         // Системные маршруты
-        '/403'  : function (params, query) {
+        '/403': function (params, query) {
             app.page = {
-                name  : '403',
-                title : 'Доступ ограничен',
+                name: '403',
+                title: 'Доступ ограничен',
                 onLoad: null,
                 params: params,
-                query : query
+                query: query,
+            };
+        },
+        '/profile': function (params, query) {
+            app.page = {
+                name: 'profile',
+                title: 'Мой профиль',
+                onLoad: function () {
+                    lemurro.profile.init();
+                },
+                params: params,
+                query: query,
             };
         },
         '/users': function (params, query) {
             app.page = {
-                name  : 'users',
-                title : 'Пользователи',
+                name: 'users',
+                title: 'Пользователи',
                 onLoad: function () {
                     lemurro.users.init();
                 },
                 params: params,
-                query : query
+                query: query,
             };
         },
 
         // Основной маршрут (должен находиться в конце)
         '/': function (params, query) {
             app.page = {
-                name  : 'dashboard',
-                title : 'Главная',
+                name: 'dashboard',
+                title: 'Главная',
                 onLoad: function () {
                     dashboard.init();
                 },
                 params: params,
-                query : query
+                query: query,
             };
-        }
-    }
+        },
+    },
 };
---
+++ src/js/profile/100-_afterShowData.js
@@ -0,0 +1,12 @@
+/**
+ * Функция обратного вызова после отображения информации
+ *
+ * @param {Object} data
+ *
+ * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 24.04.2020
+ */
+profile._afterShowData = function (data) {
+
+};
