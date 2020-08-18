7c57ea2fd4fc1f1e2bd6396073029210611c4ca6
Дмитрий Щербаков <atomcms@ya.ru>
2020-08-18 14:40:24 +0500

    feat [Users] Добавлено системное поле email

--- src/js/users/200-_collectData.js
+++ src/js/users/200-_collectData.js
@@ -3,22 +3,24 @@
  *
  * @return {object}
  *
- * @version 12.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 18.08.2020
  */
 users._collectData = function () {
     var form = $('#js-user-form');
 
     if (form.valid()) {
         return {
-            id        : form.attr('data-id'),
-            auth_id   : form.find('input[name="auth_id"]').val(),
-            roles     : lemurro.users.getRoles(form),
+            id: form.attr('data-id'),
+            auth_id: form.find('input[name="auth_id"]').val(),
+            roles: lemurro.users.getRoles(form),
             info_users: {
-                last_name  : form.find('input[name="last_name"]').val(),
-                first_name : form.find('input[name="first_name"]').val(),
-                second_name: form.find('input[name="second_name"]').val()
-            }
+                email: form.find('input[name="email"]').val(),
+                last_name: form.find('input[name="last_name"]').val(),
+                first_name: form.find('input[name="first_name"]').val(),
+                second_name: form.find('input[name="second_name"]').val(),
+            },
         };
     } else {
         swal('Внимание!', 'Заполните все поля, помеченные красным', 'warning');
